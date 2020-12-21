dd7dbd87e70214ac8f79187d823ce45a7d69c89a
Anthony R. Spyker <anthonyspyker@gmail.com>
2020-12-21 17:19:11 -0500

    first push

---
+++ script.js
@@ -0,0 +1,29 @@
+/* 1.)  functions that need to be made
+        a.) timer
+            i.)runs until a wrong question
+            ii.)runs from 90 seconds to 0.
+            iii.)starts when start button is pressed.
+            iv.)when it hits zero, goes to a screen with final score.
+        b.)A function that assigns correct answers and incorrect answers.
+        c.)A screen that you can assign your information on that hold a hi score.
+        d.)When questions are answered, you are brought to a new question.  
+    
+    2.) conditional statements
+        a.)
+
+    3.)links*/
+
+    function startTimer(){
+        
+    }
+
+
+
+
+
+
+    startButton.addEventlistner("click",startTimer)
+
+
+
+
